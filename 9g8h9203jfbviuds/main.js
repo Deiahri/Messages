137890b51a3e1e03f2687c5095051bad8ef0a43b
Junda Yin <dytlin@gmail.com>
2024-06-14 08:00:54 -0500

    Added Years and Days to timer

--- 9g8h9203jfbviuds/main.js
+++ 9g8h9203jfbviuds/main.js
@@ -21,9 +21,16 @@ function check() {
         body.classList = 'yay';
     }
     difference = Math.abs(difference);
-    let hours = Math.floor((difference)/(1000*60*60));
-    let minutes = Math.floor((difference-hours*(1000*60*60))/(1000*60));
-    let seconds = Math.floor((difference-hours*(1000*60*60)-minutes*(1000*60))/(1000));
-    timeElement.innerHTML = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    let remainder = difference;
+    let years = Math.floor(remainder/(365*24*100*60*60));
+    remainder-=years*(365*24*1000*60*60);
+    let days = Math.floor((remainder/(24*1000*60*60)));
+    remainder-=days*(24*1000*60*60);
+    let hours = Math.floor((remainder)/(1000*60*60));
+    remainder-=hours*(1000*60*60);
+    let minutes = Math.floor((remainder)/(1000*60));
+    remainder-=minutes*(1000*60);
+    let seconds = Math.floor((remainder)/(1000));
+    timeElement.innerHTML = `${String(years).padStart(2, '0')}:${String(days).padStart(2, '0')}:${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 }
 
